af4967fd580490722b1f4d007b4415871c33ac30
gadji <gadji180105@gmail.com>
2025-07-27 10:52:06 +0300

    fix(products): изменил метод рендеринка продуктов с CSR на SSR

--- src/app/chairs/[slug]/page.tsx
+++ src/app/chairs/[slug]/page.tsx
@@ -1,11 +1,53 @@
-import React from "react";
+import { ProductPage } from "@/views/product-page";
+import { instance } from "@/shared/lib/axios-instance";
+import { TypeChair } from "@/entities/product";
+import { PATCH } from "@/shared/config/pages.config";
+import { Metadata } from "next";
+import { cache } from "react";
 
-const ProductPage = ({params}: any) => {
+interface IPropsProductPageRoute {
+    params: Promise<{ slug: string }>;
+}
+
+const getProductCached = cache(async (slug: string) => {
+    try {
+        const products = await instance.get<TypeChair | null>(PATCH.CHAIR(slug));
+
+        return products.data;
+    } catch (error) {
+        console.error("Ошибка при загрузке продукта:", error);
+        return null;
+    }
+});
+
+export async function generateMetadata({ params }: IPropsProductPageRoute): Promise<Metadata> {
+    const { slug } = await params;
+    const product = await getProductCached(slug);
+
+    if (!product) {
+        return {
+            title: "Товар не найден",
+            description: "Указанный товар отсутствует в каталоге.",
+        };
+    }
+
+    return {
+        title: product.title,
+        description: product.description,
+    };
+}
+
+export default async function ProductPageRoute({ params }: IPropsProductPageRoute) {
+    const { slug } = await params;
+    const product = await getProductCached(slug);
+
+    if(!product) {
+        throw new Error("Товар не найден")
+    }
+
     return (
-        <div>
-            {params.slug}
-        </div>
+        <main>
+            <ProductPage product={product} />
+        </main>
     );
-};
-
-export default ProductPage;
+}
