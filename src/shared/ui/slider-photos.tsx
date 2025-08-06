0ba3904bf0cfaea985f50b6d34cb0b456011b5b5
Magomed Elmurzaev <ban0le4@gmail.com>
2025-08-06 04:49:48 +0300

    perf(slider): Оптимизировал слайдер. Сделал переиспользуемым компонент

--- src/shared/ui/slider-photos.tsx
+++ src/shared/ui/slider-photos.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import {
     Carousel,
@@ -11,31 +11,40 @@ import {
 import { TypeImage } from "@/entities/product/model/types";
 import Image from "next/image";
 import { clsx } from "clsx";
-import { PATCH } from "@/shared/config/pages.config";
 import { useEffect, useState } from "react";
-type typeImageCarousel = Pick<TypeImage, "id" | "url">
+
+type typeImageCarousel = Pick<TypeImage, "id" | "url">;
+
 interface IPropsSingleProductSlider {
     images: typeImageCarousel[];
+    buttons: boolean;
+    bottomSlider: boolean;
+    bottomButtons: boolean;
+    maxThumbs: number
 }
 
-const SliderPhotos = ({ images }: IPropsSingleProductSlider) => {
-    const [api, setApi] = useState<CarouselApi>();
+const SliderPhotos = ({ images, buttons = true, bottomSlider = true, bottomButtons = true, maxThumbs = 4 }: IPropsSingleProductSlider) => {
+    const [mainApi, setMainApi] = useState<CarouselApi>();
     const [current, setCurrent] = useState(0);
-
+    const [thumbsApi, setThumbsApi] = useState<CarouselApi>();
     useEffect(() => {
-        if (!api) return
+        if (!mainApi) return;
 
-        setCurrent(api.selectedScrollSnap());
+        setCurrent(mainApi.selectedScrollSnap());
 
-        api.on("select", () => {
-            setCurrent(api.selectedScrollSnap())
-        })
-    }, [api])
+        const handleSelect = () => setCurrent(mainApi.selectedScrollSnap());
+        mainApi.on("select", handleSelect);
+
+        return () => {
+            mainApi.off("select", handleSelect);
+        };
+    }, [mainApi]);
 
     return (
         <div className="relative w-[560px]">
-            <Carousel setApi={setApi}>
-                <CarouselContent >
+            {/* Основной слайдер */}
+            <Carousel setApi={setMainApi}>
+                <CarouselContent>
                     {images.map((image) => (
                         <CarouselItem key={image.id}>
                             <Image
@@ -43,31 +52,55 @@ const SliderPhotos = ({ images }: IPropsSingleProductSlider) => {
                                 height={560}
                                 src={image.url}
                                 alt={image.id + 'img'}
+                                className="object-contain"
                             />
                         </CarouselItem>
                     ))}
                 </CarouselContent>
-                <CarouselPrevious className="absolute left-2" />
-                <CarouselNext className="absolute right-2" />
+                {
+                    buttons && (
+                        <>
+                            <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2" />
+                            <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2" />
+                        </>
+                    )
+                }
             </Carousel>
-            <div className="flex gap-2">
-                {images.map((image, index) => (
-                    <Image
-                        key={index}
-                        onClick={() => api?.scrollTo(index)}
-                        className={clsx(
-                            "cursor-pointer w-[80px] h-[80px] object-cover border-2",
-                            current === index
-                                ? "border-gray-400"
-                                : "border-transparent hover:border-gray-300"
-                        )}
-                        width={60}
-                        height={60}
-                        src={image.url}
-                        alt={image.id + 'img'}
-                    />
-                ))}
-            </div>
+
+            {/* Мини-превью слайдер */}
+            {
+                bottomSlider && <div className="mt-4">
+                    <Carousel setApi={setThumbsApi}>
+                        <CarouselContent>
+                            {images.map((image, index) => (
+                                <CarouselItem key={image.id} className="basis-1/4 lg:basis-1/6">
+                                    <Image
+                                        onClick={() => mainApi?.scrollTo(index)}
+                                        className={clsx(
+                                            "cursor-pointer w-[80px] h-[80px] object-cover border-2",
+                                            current === index
+                                                ? "border-gray-400"
+                                                : "border-transparent hover:border-gray-300"
+                                        )}
+                                        width={80}
+                                        height={80}
+                                        src={image.url}
+                                        alt={image.id + 'img-thumb'}
+                                    />
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        {
+                            bottomButtons && maxThumbs > 4 && (
+                                <>
+                                    <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2" />
+                                    <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2" />
+                                </>
+                            )
+                        }
+                    </Carousel>
+                </div>
+            }
         </div>
     );
 };
